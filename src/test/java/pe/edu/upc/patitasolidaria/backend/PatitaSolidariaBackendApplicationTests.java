85b1faeb43b7bee2647a33832646342aad907f9e
Diegoroip <rosadoiporre@gmail.com>
2025-06-12 13:12:46 -0500

    Initial commit - PatitaSolidaria backend

---
+++ src/test/java/pe/edu/upc/patitasolidaria/backend/PatitaSolidariaBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.patitasolidaria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PatitaSolidariaBackendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
