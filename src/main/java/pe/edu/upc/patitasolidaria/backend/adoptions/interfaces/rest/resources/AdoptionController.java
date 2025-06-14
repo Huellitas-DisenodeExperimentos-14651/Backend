fa75bd31a0b3311ad61df9ea297394dead5b8120
rhylow <leccapedro8@gmail.com>
2025-06-14 17:03:36 -0500

    feat(api): REST endpoints for adoptions

---
+++ src/main/java/pe/edu/upc/patitasolidaria/backend/adoptions/interfaces/rest/resources/AdoptionController.java
@@ -0,0 +1,78 @@
+package pe.edu.upc.patitasolidaria.backend.adoptions.interfaces.rest.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.patitasolidaria.backend.adoptions.application.internal.AdoptionService;
+import pe.edu.upc.patitasolidaria.backend.adoptions.domain.model.Adoption;
+import pe.edu.upc.patitasolidaria.backend.adoptions.domain.model.AdoptionFilter;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/adoptions")
+@CrossOrigin(origins = "*")
+public class AdoptionController {
+
+    private final AdoptionService adoptionService;
+
+    @Autowired
+    public AdoptionController(AdoptionService adoptionService) {
+        this.adoptionService = adoptionService;
+    }
+
+    // GET
+    @GetMapping
+    public List<Adoption> getAllAdoptions(
+            @RequestParam(required = false) String gender,
+            @RequestParam(required = false) String size,
+            @RequestParam(required = false) String activity,
+            @RequestParam(required = false) String hair,
+            @RequestParam(required = false) String weight
+    ) {
+        if (gender == null && size == null && activity == null && hair == null && weight == null) {
+            return adoptionService.getAll();
+        }
+
+        AdoptionFilter filter = new AdoptionFilter();
+        filter.setGender(gender);
+        filter.setSize(size);
+        filter.setActivity(activity);
+        filter.setHair(hair);
+        filter.setWeight(weight);
+        return adoptionService.getByFilter(filter);
+    }
+
+    // POST
+    @PostMapping
+    public Adoption createAdoption(@RequestBody Adoption adoption) {
+        return adoptionService.save(adoption);
+    }
+
+    // GET
+    @GetMapping("/{id}")
+    public Adoption getAdoptionById(@PathVariable Long id) {
+        return adoptionService.getById(id);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public String deleteAdoption(@PathVariable Long id) {
+        boolean removed = adoptionService.deleteById(id);
+        if (removed) {
+            return "Mascota eliminada con éxito (id = " + id + ")";
+        } else {
+            return "Mascota no encontrada (id = " + id + ")";
+        }
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public Adoption updateAdoption(@PathVariable Long id, @RequestBody Adoption updatedAdoption) {
+        Adoption result = adoptionService.update(id, updatedAdoption);
+        if (result != null) {
+            return result;
+        } else {
+            throw new RuntimeException("Mascota no encontrada (id = " + id + ")");
+        }
+    }
+}
