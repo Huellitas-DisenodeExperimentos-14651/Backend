334f883e6862b5a9a1cf065f04b7c776e3e8dc44
Diegoroip <rosadoiporre@gmail.com>
2025-06-15 21:44:30 -0500

    feat: Cambios para deployar en local

--- src/main/java/pe/edu/upc/patitasolidaria/backend/pets/domain/model/valueobjects/PetSize.java
+++ src/main/java/pe/edu/upc/patitasolidaria/backend/pets/domain/model/valueobjects/PetSize.java
@@ -1,7 +1,20 @@
 package pe.edu.upc.patitasolidaria.backend.pets.domain.model.valueobjects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PetSize {
     SMALL,
     MEDIUM,
-    LARGE
+    LARGE;
+
+    @JsonCreator
+    public static PetSize from(String value) {
+        return PetSize.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name().toLowerCase(); // opcional: para que se serialice en minúsculas también
+    }
 }
--- src/main/java/pe/edu/upc/patitasolidaria/backend/pets/domain/model/valueobjects/PetStatus.java
+++ src/main/java/pe/edu/upc/patitasolidaria/backend/pets/domain/model/valueobjects/PetStatus.java
@@ -1,7 +1,20 @@
 package pe.edu.upc.patitasolidaria.backend.pets.domain.model.valueobjects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PetStatus {
     AVAILABLE,
     ADOPTED,
-    FOSTERED
+    FOSTERED;
+
+    @JsonCreator
+    public static PetStatus from(String value) {
+        return PetStatus.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name().toLowerCase(); // opcional
+    }
 }
