c333dd2ab2dc4a667f8dd7fad92f4e8a39b6c3dc
Diegoroip <rosadoiporre@gmail.com>
2025-06-15 22:46:25 -0500

    feat: Cambios para deployar en Render

--- src/main/java/pe/edu/upc/patitasolidaria/backend/PatitaSolidariaBackendApplication.java
+++ src/main/java/pe/edu/upc/patitasolidaria/backend/PatitaSolidariaBackendApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pe.edu.upc.patitasolidaria.backend")
 @EnableJpaAuditing
 public class PatitaSolidariaBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PatitaSolidariaBackendApplication.class, args);
 	}
-
 }
